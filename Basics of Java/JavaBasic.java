a5bb37783498369554ba9a8ea71d97a8787d575f
Pranshu Gupta <pranshugupta1423@gmail.com>
2026-02-28 14:33:55 +0530

    update

---
+++ Basics of Java/JavaBasic.java
@@ -0,0 +1,26 @@
+import java.util.*;
+public class JavaBasic {
+    public static void main(String args[]) {
+        // int a = 10;
+        // int b = 5;
+        // int sum = a + b;
+        // System.out.println(sum);
+        
+        // String input = sc.next(); //capture hojayega and will store it in input
+        // System.out.println(input);
+
+        Scanner sc = new Scanner(System.in);
+        int c= sc.nextInt();
+        System.out.println("Your given c is: ");
+        System.out.println(c);
+                
+        int d= sc.nextInt();
+        System.out.println("Your given d is: ");
+        System.out.println(d);
+        int e = c+d;
+        System.out.println("Your sum is: ");
+        System.out.println(e);
+        
+        //next nextLine nextInt nextByte nextFloat nextDouble nextBoolean nextShort nextLong
+    }
+}
